8f87c59e185fc4250c74613de040ffe3462357ad
Anubhav Yadav <ay6666@srmist.edu.in>
2024-12-07 18:25:45 +0530

    Started demo

---
+++ app.py
@@ -0,0 +1,200 @@
+import streamlit as st
+import google.generativeai as genai
+import time
+import os
+import joblib
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+new_chat_id = f'{time.time()}'
+MODEL_ROLE = 'AI Assistant'
+AI_AVATAR_ICON = '🤖'
+
+# Create a data/ folder if it doesn't already exist
+try:
+    os.mkdir('data/')
+except:
+    # data/ folder already exists
+    pass
+
+# Load past chats (if available)
+try:
+    past_chats: dict = joblib.load('data/past_chats_list')
+except:
+    past_chats = {}
+
+# Sidebar allows a list of past chats
+with st.sidebar:
+    st.write('# Past Chats')
+    if st.session_state.get('chat_id') is None:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id] + list(past_chats.keys()),
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='_',
+        )
+    else:
+        # This will happen the first time AI response comes in
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
+            index=1,
+            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
+            placeholder='_',
+        )
+    # Save new chats after a message has been sent to AI
+    # TODO: Give user a chance to name chat
+    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+
+st.write('# Chat with DostAI')
+
+# Chat history (allows to ask multiple questions)
+try:
+    st.session_state.messages = joblib.load(
+        f'data/{st.session_state.chat_id}-st_messages'
+    )
+    st.session_state.gemini_history = joblib.load(
+        f'data/{st.session_state.chat_id}-gemini_messages'
+    )
+    print('old cache')
+except:
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+    print('new_cache made')
+
+# Safety settings
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+]
+
+# System Instructions
+system_instruction = '''
+You are DostAI, an empathetic and supportive AI mental health chatbot. Your purpose is to assist users with their emotional well-being by providing a safe and understanding space for them to express themselves and receive guidance.
+
+Here are your core principles:
+
+1. Active Listening and Understanding:
+    - Carefully listen to and comprehend the user's messages, paying close attention to their emotions and concerns.
+    - Ask clarifying questions to gain a deeper understanding of their situation and feelings.
+    - Summarize the user's statements to demonstrate understanding and ensure accurate interpretation.
+
+2. Empathetic and Supportive Responses:
+    - Respond with empathy, validating the user's emotions and experiences.
+    - Offer words of encouragement and support, letting them know they are not alone.
+    - Avoid judgment or criticism, creating a safe and non-judgmental space for the user to express themselves.
+
+3. Providing Helpful Information and Suggestions:
+    - When appropriate, offer relevant information and resources related to the user's concerns.
+    - Suggest coping mechanisms, relaxation techniques, or other strategies to manage their emotions.
+    - Encourage the user to seek professional help if their needs are beyond your capabilities.
+
+4. Maintaining User Privacy and Confidentiality:
+    - Ensure all conversations are confidential and handled with utmost discretion.
+    - Avoid sharing user information with any third parties without their explicit consent.
+    - Respect the user's privacy and boundaries at all times.
+
+5. Setting Boundaries and Scope:
+    - Clearly state that you are not a replacement for professional therapy or medical advice.
+    - Encourage users to seek professional help when necessary.
+    - Avoid offering diagnoses or medical recommendations outside your expertise.
+
+Here are the link or question and answers on mental health from WHO: https://www.who.int/news-room/questions-and-answers/item/mental-health-promoting-and-protecting-human-rights
+
+Remember to be patient, understanding, and supportive in your interactions with users. Your primary goal is to create a positive and helpful experience for those seeking mental health support.
+'''
+
+st.session_state.model = genai.GenerativeModel(
+    model_name='gemini-1.5-flash',
+    safety_settings=safety_settings,
+    system_instruction=system_instruction
+)
+
+st.session_state.chat = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(
+        name=message['role'],
+        avatar=message.get('avatar'),
+    ):
+        st.markdown(message['content'])
+
+# React to user input
+if prompt := st.chat_input('Your message here...'):
+    # Save this as a chat for later
+    if st.session_state.chat_id not in past_chats.keys():
+        past_chats[st.session_state.chat_id] = st.session_state.chat_title
+        joblib.dump(past_chats, 'data/past_chats_list')
+    # Display user message in chat message container
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
+    ## Send message to AI
+    response = st.session_state.chat.send_message(
+        prompt,
+        stream=True,
+    )
+    # Display assistant response in chat message container
+    with st.chat_message(
+        name=MODEL_ROLE,
+        avatar=AI_AVATAR_ICON,
+    ):
+        message_placeholder = st.empty()
+        full_response = ''
+        assistant_response = response
+        # Streams in a chunk at a time
+        for chunk in response:
+            # Simulate stream of chunk
+            # TODO: Chunk missing `text` if API stops mid-stream ("safety"?)
+            for ch in chunk.text.split(' '):
+                full_response += ch + ' '
+                time.sleep(0.05)
+                # Rewrites with a cursor at end
+                message_placeholder.write(full_response + '▌')
+        # Write full message with placeholder
+        message_placeholder.write(full_response)
+
+    # Add assistant response to chat history
+    st.session_state.messages.append(
+        dict(
+            role=MODEL_ROLE,
+            content=st.session_state.chat.history[-1].parts[0].text,
+            avatar=AI_AVATAR_ICON,
+        )
+    )
+    st.session_state.gemini_history = st.session_state.chat.history
+    # Save to file
+    joblib.dump(
+        st.session_state.messages,
+        f'data/{st.session_state.chat_id}-st_messages',
+    )
+    joblib.dump(
+        st.session_state.gemini_history,
+        f'data/{st.session_state.chat_id}-gemini_messages',
+    )
